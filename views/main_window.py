eb4de5bb85b9a2ec169ec039496c8f5a1e440d4c
GumBen7 <gumben7@gmail.com>
2025-06-23 02:39:22 +0700

    main_window.py refactored

--- views/main_window.py
+++ views/main_window.py
@@ -1,4 +1,4 @@
-from PySide6.QtCore import Signal
+from PySide6.QtCore import Signal, Qt
 from PySide6.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QWidget
 
 
@@ -8,14 +8,23 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Zephyrus")
+        self.resize(1000, 600)
+        self.setMinimumSize(400, 250)
 
+        self._init_ui()
+        self._init_signals()
+
+    def _init_ui(self):
         self.start_button = QPushButton("Start")
 
         layout = QVBoxLayout()
-        layout.addWidget(self.start_button)
+        layout.addStretch()
+        layout.addWidget(self.start_button, alignment=Qt.AlignmentFlag.AlignCenter)
+        layout.addStretch()
 
         central_widget = QWidget()
         central_widget.setLayout(layout)
         self.setCentralWidget(central_widget)
 
+    def _init_signals(self):
         self.start_button.clicked.connect(self.start_analysis_signal.emit)
